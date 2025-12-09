ea1fc34d20200a4e252b35bf4eb4a98413e44993
mufidatuln <mufidatul.ngazizah@gmail.com>
2025-12-09 20:39:17 +0700

    Model Trainer

--- src/utils.py
+++ src/utils.py
@@ -22,16 +22,19 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e,sys)
 
-def evaluate_models(X_train,y_train,X_test,y_test, model, param):
+def evaluate_models(X_train,y_train,X_test,y_test, models, param):
     try:
         report={}
 
         for i in range(len(list(models))):
             model= list(models.values())[i]
-            para=param[list(model.keys())][i]
+            para=param[list(models.keys())[i]]
 
             gs = GridSearchCV(model,para,cv=3)
             gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
 
             # model.fit(X_train, y_train)
 
@@ -43,12 +46,10 @@ def evaluate_models(X_train,y_train,X_test,y_test, model, param):
 
             test_model_score = r2_score(y_test, y_test_pred)
 
-            report[list(model.keys())[i]]=test_model_score
+            report[list(models.keys())[i]]=test_model_score
         
         return report
     
-
-
     except Exception as e:
         raise CustomException(e, sys)
 
